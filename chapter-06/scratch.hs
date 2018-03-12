6165e94ebaf47e161f6df4342110f3d0fa96c4ef
Jake Simones <jacob.simones@gmail.com>
2018-03-11 21:12:54 -0500

    minor formatting

--- chapter-06/scratch.hs
+++ chapter-06/scratch.hs
@@ -31,12 +31,12 @@ length' (_:xs) = 1 + length' xs
 
 
 reverse' :: [a] -> [a]
-reverse' [] = []
+reverse' []     = []
 reverse' (x:xs) = reverse' xs ++ [x]
 
 
 append :: [a] -> [a] -> [a]
-append [] ys = ys
+append [] ys     = ys
 append (x:xs) ys = x : (append xs ys)
 
 

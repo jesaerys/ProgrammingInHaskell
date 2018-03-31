e06efe934338e36a6beebfee1cdd89b427284485
Jake Simones <jacob.simones@gmail.com>
2018-03-31 15:58:24 -0500

    finish ch7.1

--- chapter-07/notes.hs
+++ chapter-07/notes.hs
@@ -10,9 +10,19 @@ add' :: Int -> (Int -> Int)
 add' = \x -> (\y -> x + y)
 
 
+twice :: (a -> a) -> a -> a
+twice f x = f (f x)
+
+quadruple :: Num a => a -> a
+quadruple = twice (*2)
+
 
 tests = [
-  add 4 5 == add' 4 5
+  add 4 5 == add' 4 5,
+
+  twice (*2) 3 == 12,
+  twice reverse [1, 2, 3] == [1, 2, 3],
+  quadruple 2 == 8
   ]
 
 main :: IO ()

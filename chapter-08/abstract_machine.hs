fa2b331c540bce0a6a8b880f7d25befa28c903c0
Jake Simones <jacob.simones@gmail.com>
2018-04-28 17:14:17 -0500

    ch8.7 notes

---
+++ chapter-08/abstract_machine.hs
@@ -0,0 +1,52 @@
+module AbstractMachine where
+
+
+import Control.Exception (assert)
+
+
+data Expr = Val Int | Add Expr Expr
+
+value' :: Expr -> Int
+value' (Val n)   = n
+value' (Add x y) = value' x + value' y
+
+
+type Cont = [Op]
+
+data Op = EVAL Expr | ADD Int
+
+eval :: Expr -> Cont -> Int
+eval (Val n)   c = exec c n
+eval (Add x y) c = eval x (EVAL y : c)
+
+exec :: Cont -> Int -> Int
+exec []           n = n
+exec (EVAL y : c) n = eval y (ADD n : c)
+exec (ADD m : c)  n = exec c (m+n)
+
+value :: Expr -> Int
+value e = eval e []
+
+{-
+value (Add (Add (Val 2) (Val 3)) (Val 4))
+eval (Add (Add (Val 2) (Val 3)) (Val 4)) []
+eval (Add (Val 2) (Val 3)) [EVAL (Val 4)]
+eval (Val 2) [EVAL (Val 3), EVAL (Val 4)]
+exec [EVAL (Val 3), EVAL (Val 4)] 2
+eval (Val 3) [ADD 2, EVAL (Val 4)]
+exec [ADD 2, EVAL (Val 4)] 3
+exec [EVAL (Val 4)] 2+3
+eval (Val 4) [Add 5]
+exec [Add 5] 4
+exec [] 5+4
+9
+-}
+
+
+tests = [
+  value' (Add (Add (Val 2) (Val 3)) (Val 4)) == 9,
+  value (Add (Add (Val 2) (Val 3)) (Val 4)) == 9
+  ]
+
+main :: IO ()
+main = assert (and tests) putStrLn "OK"
